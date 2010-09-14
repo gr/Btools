d1db887cceaacfe73a1a22941b2d002860ed12f8
George Regentov <george.regentov@gmail.com>
2010-09-14 16:54:15 +0400

    1 issue

--- admin.py
+++ admin.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-
 from django.contrib import admin
-from models import BookSet
+from models import *
 
-class BookSetAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'query_type', 'query', 'about']
-    list_filter = ['db', 'query_type', 'view_me']
-    ordering = ['name', 'time']
-    list_display = ('name', 'view_me', 'db', 'query_type', 'query', 'time', 'last_time_update', 'number_of_books')
+class BookDBAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'host', 'port', 'db', 'encoding', 'about']
+    list_filter = ['name', 'host', 'port', 'db', 'encoding', 'about']
+    ordering = ['name', 'host', 'port', 'db', 'encoding', 'about']
+    list_display = ('name', 'host', 'port', 'db', 'encoding')
 
 
-admin.site.register(BookSet, BookSetAdmin)
+admin.site.register(BookDB, BookDBAdmin)
