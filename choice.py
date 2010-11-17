2422526893f53fb87c461ab8f57cebac1b74749c
George <george@gregentov.(none)>
2010-11-17 23:25:39 +0300

    lork in

---
+++ choice.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+QUERY_TYPE_CHOICES = (('CCL', 'CCL'), 
+                      ('S-CCL','S-CCL'), 
+                      ('CQL','CQL'), 
+                      ('S-CQL','S-CQL'), 
+                      ('PQF','PQF'), 
+                      ('C2','C2'), 
+                      ('ZSQL','ZSQL'), 
+                      ('CQL-TREE','CQL-TREE'))
+                                            
+SYNTAX_CHOICES = (('RUSMARC', 'RUSMARC'), 
+                  ('USMARC', 'USMARC'), 
+                  ('USMARCnonstrict', 'USMARCnonstrict'), 
+                  ('UKMARC', 'UKMARC'), 
+                  ('SUTRS', 'SUTRS'), 
+                  ('XML', 'XML'),
+                  ('SGML', 'SGML'),
+                  ('GRS-1', 'GRS-1'),
+                  ('OPAC', 'OPAC'),
+                  ('EXPLAIN', 'EXPLAIN'))
